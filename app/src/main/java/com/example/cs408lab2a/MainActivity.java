7339c7c6e52ce4f08c0edc93a465859264c44574
Charlie Owen <cowen6@stu.jsu.edu>
2024-02-01 14:18:20 -0600

    Working on Algorithm Implementation

--- app/src/main/java/com/example/cs408lab2a/MainActivity.java
+++ app/src/main/java/com/example/cs408lab2a/MainActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.example.cs408lab2a.databinding.ActivityMainBinding;
 
 import java.text.NumberFormat;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +28,42 @@ public class MainActivity extends AppCompatActivity {
         binding.calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check if inputs have values
+                //check if inputs have values before creating bindings?
+                //app works when all values have text
+                //app shuts down if user fails to provide values
 
                 EditText billInput = binding.billInput;
                 EditText tipInput = binding.tipInput;
                 EditText peopleInput = binding.peopleInput;
                 TextView t = binding.resultOutput;
+                String output;
 
-                float bill = Float.parseFloat(billInput.toString());
-                float tip = 1 + ((float) Integer.parseInt(tipInput.toString()) / 100);
-                int people = Integer.parseInt(peopleInput.toString());
+                Log.d(TAG, "bill input: " + billInput.getText().toString());
+                Log.d(TAG, "tip input: " + tipInput.getText().toString());
+                Log.d(TAG, "people input: " + peopleInput.getText().toString());
 
-                float total = bill * tip;
-                float result = total / people;
+                boolean billempty = billInput.getText().toString().isEmpty();
+                boolean tipempty = billInput.getText().toString().isEmpty();
+                boolean peopleempty = billInput.getText().toString().isEmpty();
 
-                String output = label + format.format(result);
+                Log.d(TAG, "bill empty: " + billempty);
+                Log.d(TAG, "tip empty: " + tipempty);
+                Log.d(TAG, "people empty: " + peopleempty);
+
+                if(!billempty && !tipempty && !peopleempty){
+                    float bill = Float.parseFloat(billInput.getText().toString());
+                    float tip = 1 + ((float) Integer.parseInt(tipInput.getText().toString()) / 100);
+                    int people = Integer.parseInt(peopleInput.getText().toString());
+
+                    float total = bill * tip;
+                    float result = total / people;
+
+                    output = label + format.format(result);
+                }
+                else {
+                    output = label;
+                }
+
                 t.setText(output);
             }
         });
